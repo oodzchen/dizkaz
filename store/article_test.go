dbfb547733e5e2b392b40468dfd731b17d50a08a
Kholin <oodzchen@gmail.com>
2025-05-03 18:23:32 +0800

    chore: skip unit tests with db connections

--- store/article_test.go
+++ store/article_test.go
@@ -1,152 +1,141 @@
 package store
 
-import (
-	"log"
-	"testing"
-	"time"
+// func connectDB(appCfg *config.AppConfig) (*pgstore.PGStore, error) {
+// 	pg := pgstore.New(&pgstore.DBConfig{
+// 		DSN: appCfg.DB.GetDSN(),
+// 	})
 
-	"github.com/oodzchen/dizkaz/config"
-	mt "github.com/oodzchen/dizkaz/mocktool"
-	"github.com/oodzchen/dizkaz/store/pgstore"
-	"golang.org/x/crypto/bcrypt"
-)
+// 	err := pg.ConnectDB()
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-func connectDB(appCfg *config.AppConfig) (*pgstore.PGStore, error) {
-	pg := pgstore.New(&pgstore.DBConfig{
-		DSN: appCfg.DB.GetDSN(),
-	})
+// 	return pg, nil
+// }
 
-	err := pg.ConnectDB()
-	if err != nil {
-		return nil, err
-	}
+// func registerNewUser(store *Store, appCfg *config.AppConfig) (int, error) {
+// 	user := mt.GenUser()
+// 	pwd, _ := bcrypt.GenerateFromPassword([]byte(appCfg.DB.UserDefaultPassword), 10)
+// 	return store.User.Create(user.Email, string(pwd), user.Name, "common_user")
+// }
 
-	return pg, nil
-}
+// func createNewArticle(store *Store, userId int) (int, error) {
+// 	article := mt.GenArticle()
+// 	return store.Article.Create(article.Title, "", article.Content, userId, 0, "general", time.Now(), false)
+// }
 
-func registerNewUser(store *Store, appCfg *config.AppConfig) (int, error) {
-	user := mt.GenUser()
-	pwd, _ := bcrypt.GenerateFromPassword([]byte(appCfg.DB.UserDefaultPassword), 10)
-	return store.User.Create(user.Email, string(pwd), user.Name, "common_user")
-}
+// func TestArticleVote(t *testing.T) {
+// 	appCfg, err := config.NewTest()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-func createNewArticle(store *Store, userId int) (int, error) {
-	article := mt.GenArticle()
-	return store.Article.Create(article.Title, "", article.Content, userId, 0, "general", time.Now(), false)
-}
+// 	// fmt.Println("testing db config:", appCfg.DB)
 
-func TestArticleVote(t *testing.T) {
-	appCfg, err := config.NewTest()
-	if err != nil {
-		log.Fatal(err)
-	}
+// 	pg, err := connectDB(appCfg)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer pg.CloseDB()
 
-	// fmt.Println("testing db config:", appCfg.DB)
+// 	err = pg.InitModules()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	pg, err := connectDB(appCfg)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer pg.CloseDB()
+// 	store := New(pg.Article, pg.User, pg.Role, pg.Permission, pg.Activity, pg.Message, pg.Category)
 
-	err = pg.InitModules()
-	if err != nil {
-		log.Fatal(err)
-	}
+// 	uId, err := registerNewUser(store, appCfg)
+// 	mt.LogFailed(err)
 
-	store := New(pg.Article, pg.User, pg.Role, pg.Permission, pg.Activity, pg.Message, pg.Category)
+// 	aId, err := createNewArticle(store, uId)
+// 	mt.LogFailed(err)
 
-	uId, err := registerNewUser(store, appCfg)
-	mt.LogFailed(err)
+// 	uBId, err := registerNewUser(store, appCfg)
+// 	mt.LogFailed(err)
 
-	aId, err := createNewArticle(store, uId)
-	mt.LogFailed(err)
+// 	// fmt.Println("uId: ", uId)
+// 	// fmt.Println("aId: ", aId)
+// 	// fmt.Println("uBId: ", uBId)
 
-	uBId, err := registerNewUser(store, appCfg)
-	mt.LogFailed(err)
+// 	t.Run("Vote up", func(t *testing.T) {
+// 		_, err = store.Article.ToggleVote(aId, uBId, "up")
+// 		if err != nil {
+// 			t.Errorf("should vote up success but got %v", err)
+// 		}
+// 	})
 
-	// fmt.Println("uId: ", uId)
-	// fmt.Println("aId: ", aId)
-	// fmt.Println("uBId: ", uBId)
+// 	t.Run("Change vote to down", func(t *testing.T) {
+// 		_, err = store.Article.ToggleVote(aId, uBId, "down")
+// 		if err != nil {
+// 			t.Errorf("should vote down success but got %v", err)
+// 		}
+// 	})
 
-	t.Run("Vote up", func(t *testing.T) {
-		_, err = store.Article.ToggleVote(aId, uBId, "up")
-		if err != nil {
-			t.Errorf("should vote up success but got %v", err)
-		}
-	})
+// 	t.Run("Revoke vote", func(t *testing.T) {
+// 		_, err = store.Article.ToggleVote(aId, uBId, "down")
+// 		if err != nil {
+// 			t.Errorf("should revoke vote success but got %v", err)
+// 		}
+// 	})
+// }
 
-	t.Run("Change vote to down", func(t *testing.T) {
-		_, err = store.Article.ToggleVote(aId, uBId, "down")
-		if err != nil {
-			t.Errorf("should vote down success but got %v", err)
-		}
-	})
+// func TestArticleCheckVote(t *testing.T) {
+// 	appCfg, err := config.NewTest()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	t.Run("Revoke vote", func(t *testing.T) {
-		_, err = store.Article.ToggleVote(aId, uBId, "down")
-		if err != nil {
-			t.Errorf("should revoke vote success but got %v", err)
-		}
-	})
-}
+// 	pg, err := connectDB(appCfg)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer pg.CloseDB()
 
-func TestArticleCheckVote(t *testing.T) {
-	appCfg, err := config.NewTest()
-	if err != nil {
-		log.Fatal(err)
-	}
+// 	err = pg.InitModules()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	pg, err := connectDB(appCfg)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer pg.CloseDB()
+// 	store := &Store{
+// 		Activity:   pg.Activity,
+// 		Article:    pg.Article,
+// 		Message:    pg.Message,
+// 		Permission: pg.Permission,
+// 		Role:       pg.Role,
+// 		User:       pg.User,
+// 	}
 
-	err = pg.InitModules()
-	if err != nil {
-		log.Fatal(err)
-	}
+// 	uId, err := registerNewUser(store, appCfg)
+// 	mt.LogFailed(err)
 
-	store := &Store{
-		Activity:   pg.Activity,
-		Article:    pg.Article,
-		Message:    pg.Message,
-		Permission: pg.Permission,
-		Role:       pg.Role,
-		User:       pg.User,
-	}
+// 	aId, err := createNewArticle(store, uId)
+// 	mt.LogFailed(err)
 
-	uId, err := registerNewUser(store, appCfg)
-	mt.LogFailed(err)
+// 	t.Run("Check unvote article", func(t *testing.T) {
+// 		err, voteType := store.Article.VoteCheck(aId, uId)
+// 		if err != nil {
+// 			t.Errorf("vote check error %v", err)
+// 		}
+// 		// fmt.Println("vote check result: ", err)
+// 		if voteType != "up" {
+// 			t.Errorf("vote check type should be %s, but got %v", "up", voteType)
+// 		}
+// 	})
 
-	aId, err := createNewArticle(store, uId)
-	mt.LogFailed(err)
+// 	t.Run("Check voted article", func(t *testing.T) {
+// 		_, err = store.Article.ToggleVote(aId, uId, "down")
+// 		if err != nil {
+// 			t.Errorf("vote down article failed: %v", err)
+// 		}
 
-	t.Run("Check unvote article", func(t *testing.T) {
-		err, voteType := store.Article.VoteCheck(aId, uId)
-		if err != nil {
-			t.Errorf("vote check error %v", err)
-		}
-		// fmt.Println("vote check result: ", err)
-		if voteType != "up" {
-			t.Errorf("vote check type should be %s, but got %v", "up", voteType)
-		}
-	})
+// 		err, vt := store.Article.VoteCheck(aId, uId)
+// 		if err != nil {
+// 			t.Errorf("should check with no error, but got: %v", err)
+// 		}
 
-	t.Run("Check voted article", func(t *testing.T) {
-		_, err = store.Article.ToggleVote(aId, uId, "down")
-		if err != nil {
-			t.Errorf("vote down article failed: %v", err)
-		}
-
-		err, vt := store.Article.VoteCheck(aId, uId)
-		if err != nil {
-			t.Errorf("should check with no error, but got: %v", err)
-		}
-
-		if vt != "down" {
-			t.Errorf("should get vote type as 'down' but got %s", vt)
-		}
-	})
-}
+// 		if vt != "down" {
+// 			t.Errorf("should get vote type as 'down' but got %s", vt)
+// 		}
+// 	})
+// }
